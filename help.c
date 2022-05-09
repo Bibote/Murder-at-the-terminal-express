840ebfb7acd8b1cdb8f6421a042d61f4d0a833eb
Iturri12 <100165885+Iturri12@users.noreply.github.com>
2022-05-09 17:19:17 +0200

    Add files via upload

---
+++ help.c
@@ -0,0 +1,11 @@
+int main()
+{
+    char str[100];
+    printf("A list of the usable commands:")
+    printf("ls: Used to lookaround objects ");
+    printf("pwd: Shows working directory ");
+    printf("talk: Allows to talk with the different people");
+    printf("go: Used to move to next directory");
+    printf("wheretogo: Shows which directories you can move to");
+    return 0;
+    }
