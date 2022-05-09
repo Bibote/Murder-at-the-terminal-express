1776662ea692248c1928fcb5cc67890dadd46fb6
Iturri12 <100165885+Iturri12@users.noreply.github.com>
2022-05-09 17:37:47 +0200

    Update help.c

--- help.c
+++ help.c
@@ -1,11 +1,12 @@
-int main()
-{
-    char str[100];
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+void main(void){
     printf("A list of the usable commands:")
-    printf("lookaround: Used to lookaround objects ");
+    printf("ls: Used to lookaround objects ");
     printf("pwd: Shows working directory ");
     printf("talk: Allows to talk with the different people");
     printf("go: Used to move to next directory");
     printf("wheretogo: Shows which directories you can move to");
-    return 0;
     }
+    
