97cb6752954d12e00de8b29482b7df389f923106
Iturri12 <100165885+Iturri12@users.noreply.github.com>
2022-05-09 17:20:14 +0200

    Update help.c

--- help.c
+++ help.c
@@ -2,7 +2,7 @@ int main()
 {
     char str[100];
     printf("A list of the usable commands:")
-    printf("ls: Used to lookaround objects ");
+    printf("lookaround: Used to lookaround objects ");
     printf("pwd: Shows working directory ");
     printf("talk: Allows to talk with the different people");
     printf("go: Used to move to next directory");
