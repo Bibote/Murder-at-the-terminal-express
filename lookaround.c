d94c56b1b1fd982ca3dbc1283d809dd3d44243b3
ahmedsabouni <ahmed.sabouni11@gmail.com>
2022-05-09 17:05:35 +0200

    lookaround

--- lookaround.c
+++ lookaround.c
@@ -1,48 +1,44 @@
-#include <sys/types.h>
+#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include<sys/wait.h>
 #include <dirent.h>
+#define MAX_PATH 200
 
-int lookAround(char dirname[], char const *ext)
-
+int main(int argc, char const *argv[])
 {
-    printf("In this room you can see:");
-    DIR * d;
-    struct dirent *dir;
-
-    d = opendir(dirname);
-
-    if (d != NULL)
-    {
-        while ((dir = readdir(d)) != NULL)
-        {
-            if (has_extension(dir->d_name, ext))
-            {
-                printf("%s\n", dir->d_name);
-            }
+    
+    // get grandparent directory
+    char s1[MAX_PATH];
+    getcwd(s1,MAX_PATH);
+    
+    DIR *dir;
+    struct dirent *ent;
+    dir = opendir(s1);
+    if (dir == NULL) {
+        printf("Could not open current directory\n");
+        return -1;
+    }
+    while ((ent = readdir(dir)) != NULL) {
+        
+        if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0) {
+            // verify if it is a directory
+            if (ent->d_type != DT_DIR) {
+            
+            printf("%s\n", ent->d_name);}
         }
-        closedir(d);
     }
-    return(0);
-}
+    closedir(dir);
+
+    
+
+    
 
 
-const char* acutalDir(){
-    char path[] = pwd();
 
-    char linea[PATH_MAX];
-    getcwd(linea, PATH_MAX);
-    //char *realpath(const char *path, char *resolved_path);
-
-    //Directorio actual char *getcwd(char *buf, size_t size);
-
-    /*int i = 0;
-    char *p = strtok (path, "/");
-    char *array[252];
-
-    while (p != NULL)
-    {
-        array[i++] = p;
-        p = strtok (NULL, "/");
-    }
-    return array[i];*/
-    return path;
+    
+    return 0;
 }
