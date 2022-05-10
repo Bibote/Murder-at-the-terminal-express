a588d9f8c638122fb5a4c8ae27f8a53e885fcb4b
Iturri12 <alvaroiturrizaa@gmail.com>
2022-05-10 20:59:56 +0200

    changes

--- help.c
+++ help.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <limits.h>
 void main(void){
-    printf("A list of the usable commands:");
-    printf("ls: Used to lookaround objects ");
-    printf("pwd: Shows working directory ");
-    printf("talk: Allows to talk with the different people");
-    printf("go: Used to move to next directory");
-    printf("wheretogo: Shows which directories you can move to");
+    printf("A list of the usable commands:\n");
+    printf("ls: Used to lookaround objects\n");
+    printf("pwd: Shows working directory\n");
+    printf("talk: Allows to talk with the different people\n");
+    printf("go: Used to move to next directory\n");
+    printf("wheretogo: Shows which directories you can move to\n");
     }
     
