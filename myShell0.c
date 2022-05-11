ea12aa2efa34dc53bfdf2494eeaa8d2ebc503f90
agusalex02 <alexagustin@jmiparragirre.eus>
2022-05-11 16:35:43 +0200

    Moved record history to the shell and added the train driver

--- myShell0.c
+++ myShell0.c
@@ -7,6 +7,11 @@
 #include <string.h>
 #include <errno.h>
 #include<sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
 #include"go.c"
 #include"init.c"
 
@@ -133,8 +138,24 @@ int main ()
          if((!strcomp(args[0],"history"))&&argc>1){
             getcwd(PREV_PATH,sizeof(PREV_PATH)); 
             chdir(HISTPATH); 
-            char commandPath[456];
-            execute()
+
+            char path[300]; 
+            getcwd(path,300); 
+            strcat(path,"/history.txt"); 
+
+            int fd = open(path,O_RDWR|O_APPEND|O_CREAT, 0644); 
+            if(fd>0){ 
+               char *command[50]; 
+               //command = (char)malloc(sizeof(argv)*sizeof(char));
+
+               for(int i=0; i<argc;i++){ //puts the arguments written on the terminal into an array (command)
+                  strcat(command,argv[i]); 
+                  strcat(command," "); 
+               }  
+               write(fd,command,strlen(command)); //writes the command on the history
+               write(fd,"\n",sizeof("\n")); 
+               close(fd); 
+            }
          }
          execute(argc, args);
          
