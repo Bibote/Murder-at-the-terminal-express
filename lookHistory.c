e0ea61e53a60cfed18ca5af8474850ebd33009b1
Iturri12 <alvaroiturrizaa@gmail.com>
2022-05-14 18:35:21 +0200

    fixing bugs

--- lookHistory.c
+++ lookHistory.c
@@ -8,18 +8,26 @@
 #define MAX_PATH 200
 int main(int argc, char* argv[])
 {
-    if (argv[1]=="help"){
-        printf(\033[0;32m);
+    //First we will search to see if the user is calling for help in the function, and show in the console how to use the function.
+    if(argc==2){
+        if(!strcmp(argv[1], "help")) {
+        printf("\033[0;32m");
         printf("This command only needs one argument to be used: lookHistory. It shows all the commands tha player has written on the terminal, even if they are not real commands.");
-        printf(\033[0m);
+        printf("\033[0m");
+         return 1;
+         }else{
+            printf("Wrong ammount of parametersss");
+            return 0;
+        }
     }
-    if(argc!=2){
-        printf(\033[0;3m);
+    if(argc!=1){
+        printf("\033[0;3m");
         printf("Wrong ammount of parameters");
         printf("\n");
-        printf(\033[0m);
+        printf("\033[0m");
         return 0;
     }
+    
     char*p;
     char cwd[MAX_PATH];
     char route[80];
@@ -51,19 +59,19 @@ int main(int argc, char* argv[])
     stat(route, &sfile);
     fd = open(route,O_RDONLY);
     if (fd == -1) {
-        printf(\033[0;3m);
+        printf("\033[0;3m");
         printf("There has been an error with the history");
         printf("\n");
-        printf(\033[0m);
+        printf("\033[0m");
         return 1;
     }
     int i=sfile.st_size;
     static char buff[100];
 
     if(read(fd,buff,i)==-1) {
-        printf(\033[0;3m);
+        printf("\033[0;3m");
         printf("error");
-        printf(\033[0m);
+        printf("\033[0m");
     }
     else {
         printf("%s",buff);
