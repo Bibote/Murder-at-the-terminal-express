7017732572d548efb8b58a15e99afa87845a67fb
Iturri12 <alvaroiturrizaa@gmail.com>
2022-05-16 16:07:30 +0200

    added isolate

--- help.c
+++ help.c
@@ -11,6 +11,7 @@ int main(int argc, char* argv[]){
     return 0;
     }
 
+    printf("The goal of the game is to go around the train finding clues and talking to people to discover who the killer is\n\n");
     printf("Available commands:\n\n");
     printf("lookaround: To see if there is something to interact with \n");
     printf("wheretogo: if you are lost or search for new places\n");
@@ -18,8 +19,9 @@ int main(int argc, char* argv[]){
     printf("go: Use it to move from a place to an other\n");
     printf("lookHistory: Prints a list of the used commands \n");
     printf("interact: Use to interacts with the different objects and people around the room\n");
+    printf("isolate: Move a person to a room, in order to interrogate\n");
     printf("help: to see this message again\n\n");
-    printf("You can also type a specific command followed by help, and it will show a brief explanation of how to use each command (Example: talk help)\n");
+    printf("You can also type a specific command followed by help, and it will show a brief explanation of how to use each command (Example: talk help)\n\n");
     printf("press enter to continue you investigation\n");
     getchar();
     system("clear");
