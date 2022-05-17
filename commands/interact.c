42c706042b19675b7cb67bc9a2f4710f0eb9e507
Iturri12 <alvaroiturrizaa@gmail.com>
2022-05-17 21:11:05 +0200

    cannot interact with people

--- commands/interact.c
+++ commands/interact.c
@@ -27,6 +27,13 @@ int main(int argc, char* argv[])
     return 1;
     }
    
+   if(!strcmp(argv[1], "Alvaro" || "Wiame" || "Millan" || "Edurne" || "Ahmed" || "Alex" )) {
+    printf("\033[0;32m");
+    printf("You cant interact with people, use the talk command");
+    printf("\n");
+    printf("\033[0m");
+    return 1;
+    }
 
     int fd;
 
